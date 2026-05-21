2bf6eab730f2085fbfc005b073ddbe1618e42e06
Augusto Abel Karacsany <augusto.karacsany@gmail.com>
2026-05-21 19:56:01 -0300

    Feat: Implementar modelo de entidad Habitacion con encapsulamiento.
    - Se estructuran las propiedades de Habitacion ( Como el id, el numero y su estado ( Half, Completa, Etc... ). ) en la capa lógica.

--- src/BLL/Habitacion.java
+++ src/BLL/Habitacion.java
@@ -1,5 +1,23 @@
 package BLL;
 
 public class Habitacion {
+    private int id;
+    private String numero;
+    private EstadoHabitacion estado;
 
+    public Habitacion(int id, String numero, EstadoHabitacion estado) {
+        this.id = id;
+        this.numero = numero;
+        this.estado = estado;
+    }
+
+    // Getters y Setters establecidos, no tocar.
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNumero() { return numero; }
+    public void setNumero(String numero) { this.numero = numero; }
+
+    public EstadoHabitacion getEstado() { return estado; }
+    public void setEstado(EstadoHabitacion estado) { this.estado = estado; }
 }
