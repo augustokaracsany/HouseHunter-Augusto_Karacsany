616135ec194d6e0b6fec2e29704347c2483506d1
Augusto Abel Karacsany <augusto.karacsany@gmail.com>
2026-06-04 11:57:02 -0300

    Database: Expansión de datos de prueba en script SQL.
    - Limpieza de comandos ALTER obsoletos o ya ejecutados en la estructura.
    - Agregadas nuevas entidades corporativas ( MercadoLibre y Accenture. ) con sus respectivos datos e identificadores.
    - Registradas nuevas reservas válidas y cronogramas de actividades detallados para el entorno de simulación.
    - Incorporados nuevos invitados con tokens asignados para testing cruzado en el login.
    - Pobladas tablas de asignaciones de habitaciones y control de asistencia real a actividades para robustecer los reportes visuales.

--- sql/househunter.sql
+++ sql/househunter.sql
@@ -3,13 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-06-2026 a las 02:13:38
+-- Tiempo de generación: 04-06-2026 a las 16:53:37
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -41,10 +42,12 @@ CREATE TABLE `actividades` (
 --
 
 INSERT INTO `actividades` (`id`, `id_reserva`, `nombre`, `descripcion`, `importancia`, `categoria`, `hora_actividad`) VALUES
-(1, 1, 'Keynote de Apertura: Innovación AI', 'Presentación de las metas tecnológicas y bienvenida institucional.', 'ALTA', 'CHARLAS', '09:00:00'),
-(2, 1, 'Workshop Técnico: Java Avanzado', 'Laboratorio práctico interactivo sobre concurrencia y JPA.', 'MEDIA', 'COWORK', '14:30:00'),
-(3, 1, 'After Office & Networking', 'Espacio libre de recreación con barra de tragos y música en vivo.', 'BAJA', 'RECREACION', '19:00:00'),
-(4, 1, 'TEST', 'DESCTEST', 'ALTA', 'OTROS', '12:30:05');
+(6, 1, 'Keynote de Apertura: Innovación AI', 'Presentación de las metas tecnológicas y bienvenida institucional.', 'ALTA', 'CHARLAS', '09:00:00'),
+(7, 1, 'TEST', 'DESCTEST', 'ALTA', 'OTROS', '12:30:05'),
+(8, 1, 'Workshop Técnico: Java Avanzado', 'Laboratorio práctico interactivo sobre concurrencia y JPA.', 'MEDIA', 'COWORK', '14:30:00'),
+(9, 1, 'After Office & Networking', 'Espacio libre de recreación con barra de tragos y música en vivo.', 'BAJA', 'RECREACION', '19:00:00'),
+(10, 1, 'TEST2', '', 'ALTA', 'CHARLAS', '19:20:00'),
+(12, 2, 'TEST3', 'Recopilación todos los Choques de Franco.', 'ALTA', 'RECREACION', '20:30:00');
 
 -- --------------------------------------------------------
 
@@ -67,7 +70,8 @@ CREATE TABLE `asignaciones_habitaciones` (
 
 INSERT INTO `asignaciones_habitaciones` (`id`, `id_reserva`, `id_habitacion`, `id_usuario`, `casillero_checkin`, `fecha_registro`) VALUES
 (1, 1, 1, 1, '1', '2026-05-28 14:49:55'),
-(2, 1, 1, 3, '2', '2026-05-28 14:50:41');
+(2, 1, 1, 3, '2', '2026-05-28 14:50:41'),
+(3, 2, 2, 4, '1', '2026-06-04 14:49:21');
 
 -- --------------------------------------------------------
 
@@ -87,7 +91,7 @@ CREATE TABLE `asistencias_actividades` (
 --
 
 INSERT INTO `asistencias_actividades` (`id`, `id_actividad`, `id_usuario`, `asistio`) VALUES
-(1, 4, 3, 'S');
+(2, 12, 4, 'S');
 
 -- --------------------------------------------------------
 
@@ -132,7 +136,8 @@ INSERT INTO `datos_personas` (`id_usuario`, `dni`, `nombre`, `apellido`, `celula
 (4, '44728397', 'Franco', 'Colapinto', NULL),
 (6, '12345678', 'Robert', 'Trebor', NULL),
 (8, '99888777', 'Alfonso', 'Gutierrez', '4745450955'),
-(9, '11222333', 'Eustaquio', 'Ramirez', NULL);
+(9, '11222333', 'Eustaquio', 'Ramirez', NULL),
+(10, '12345666', 'Michael', 'Schumacher', NULL);
 
 -- --------------------------------------------------------
 
@@ -152,7 +157,7 @@ CREATE TABLE `habitaciones` (
 
 INSERT INTO `habitaciones` (`id`, `numero`, `estado`) VALUES
 (1, '101', 'Completa'),
-(2, '102', 'Libre'),
+(2, '102', 'Half'),
 (3, '103', 'Libre'),
 (4, '201', 'Libre'),
 (5, '202', 'Libre'),
@@ -172,19 +177,22 @@ CREATE TABLE `lista_invitados_previa` (
   `apellido` varchar(100) NOT NULL,
   `celular` varchar(20) NOT NULL,
   `dni_companero` varchar(15) NOT NULL,
-  `token_acceso` varchar(255) DEFAULT NULL
+  `token_acceso` varchar(255) DEFAULT NULL,
+  `asistencia_confirmada` char(1) NOT NULL DEFAULT 'N'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `lista_invitados_previa`
 --
 
-INSERT INTO `lista_invitados_previa` (`id`, `id_reserva`, `dni`, `nombre`, `apellido`, `celular`, `dni_companero`, `token_acceso`) VALUES
-(1, 1, '44555666', 'Luca', 'Borrelli', '1122334455', '44728397', '2377'),
-(2, 1, '44728397', 'Franco', 'Colapinto', '1155667788', '44555666', '1862'),
-(3, 1, '12345678', 'Robert', 'Trebor', '1199887766', '0', '8454'),
-(4, 1, '99888777', 'Alfonso Gutierrez', '', '4745450955', '', '8033'),
-(5, 1, '11222333', 'Eustaquio Ramirez', '', '', '', '7052');
+INSERT INTO `lista_invitados_previa` (`id`, `id_reserva`, `dni`, `nombre`, `apellido`, `celular`, `dni_companero`, `token_acceso`, `asistencia_confirmada`) VALUES
+(1, 1, '44555666', 'Luca', 'Borrelli', '1122334455', '44728397', '2281', 'N'),
+(2, 1, '44728397', 'Franco', 'Colapinto', '1155667788', '44555666', '4728', 'N'),
+(3, 1, '12345678', 'Robert', 'Trebor', '1199887766', '0', '6890', 'N'),
+(4, 1, '99888777', 'Alfonso Gutierrez', '', '4745450955', '', '3510', 'N'),
+(5, 1, '11222333', 'Eustaquio Ramirez', '', '', '', '1695', 'N'),
+(6, 2, '47299224', 'Augusto Karacsany', '', '', '', '6311', 'N'),
+(7, 2, '44728397', 'Franco Colapinto', '', '', '', '3019', 'S');
 
 -- --------------------------------------------------------
 
@@ -277,7 +285,8 @@ CREATE TABLE `reservas_hotel` (
 --
 
 INSERT INTO `reservas_hotel` (`id`, `id_empresa`, `codigo_unico_evento`, `fecha_inicio`, `fecha_fin`, `cantidad_estimada_asistentes`, `id_plantilla`, `created_at`) VALUES
-(1, 2, 'GLOBANT-2026', '2026-06-01', '2026-06-05', 50, NULL, '2026-05-26 00:41:42');
+(1, 2, 'GLOBANT-2026', '2026-06-01', '2026-06-05', 50, 1, '2026-05-26 00:41:42'),
+(2, 2, 'GLOBANTX', '2027-05-20', '2027-05-25', 30, NULL, '2026-06-01 01:57:40');
 
 -- --------------------------------------------------------
 
@@ -289,21 +298,24 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `rol` enum('ADMINISTRADOR','EMPRESA','INVITADO') NOT NULL
+  `rol` enum('ADMINISTRADOR','EMPRESA','INVITADO') NOT NULL,
+  `asistencia_confirmada` char(1) NOT NULL DEFAULT 'N',
+  `fecha_confirmacion` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `email`, `password`, `rol`) VALUES
-(1, 'admin@hotel.com', '$2a$10$uF/66Rb9xmS8jD3cSVtP.eXIaRujgfBi9KD8e68y9Zd3NcgD6i83i', 'ADMINISTRADOR'),
-(2, 'info@globant.com', '$2a$10$wjR9D2Q5HYWx41yfG89LE.PQDi.FF7WE7YNxHw4IMbxD9rH12MurC', 'EMPRESA'),
-(3, 'invitado@gmail.com', '$2a$10$q/ABRKqFBznwHcrPci3st.rAjlfxQkZMzrAHGXmSi7zjPMFkUHCo6', 'INVITADO'),
-(4, 'invitado1@gmail.com', '$2a$10$tHd/HAtSKXNyhggL5TYfYe5HcYWe1gA04cLpY0OJG2FIYxMz7u2bm', 'INVITADO'),
-(6, 'Robert@gmail.com', '$2a$10$vQNuD3WE0qG5r.yeGE2Bvu1qMRJKvzS/aXuz0y0nNJOsRxwc5m94i', 'INVITADO'),
-(8, 'invitado9@gmail.com', '$2a$10$DdEv1MMiV7y4lIY9V4/arO8PUdc5PaOg5O2il.MnxbOZ3Blb1kxpy', 'INVITADO'),
-(9, 'invitado10@gmail.com', '$2a$10$sxGcXNaIN2aI5C3z5knUJuLmkSbYeU.Gp/Akh/06VcyZAJlm1S9Uu', 'INVITADO');
+INSERT INTO `usuarios` (`id`, `email`, `password`, `rol`, `asistencia_confirmada`, `fecha_confirmacion`) VALUES
+(1, 'admin@hotel.com', '$2a$10$uF/66Rb9xmS8jD3cSVtP.eXIaRujgfBi9KD8e68y9Zd3NcgD6i83i', 'ADMINISTRADOR', 'N', NULL),
+(2, 'info@globant.com', '$2a$10$wjR9D2Q5HYWx41yfG89LE.PQDi.FF7WE7YNxHw4IMbxD9rH12MurC', 'EMPRESA', 'N', NULL),
+(3, 'invitado@gmail.com', '$2a$10$q/ABRKqFBznwHcrPci3st.rAjlfxQkZMzrAHGXmSi7zjPMFkUHCo6', 'INVITADO', 'N', NULL),
+(4, 'invitado1@gmail.com', '$2a$10$tHd/HAtSKXNyhggL5TYfYe5HcYWe1gA04cLpY0OJG2FIYxMz7u2bm', 'INVITADO', 'N', NULL),
+(6, 'Robert@gmail.com', '$2a$10$vQNuD3WE0qG5r.yeGE2Bvu1qMRJKvzS/aXuz0y0nNJOsRxwc5m94i', 'INVITADO', 'N', NULL),
+(8, 'invitado9@gmail.com', '$2a$10$DdEv1MMiV7y4lIY9V4/arO8PUdc5PaOg5O2il.MnxbOZ3Blb1kxpy', 'INVITADO', 'N', NULL),
+(9, 'invitado10@gmail.com', '$2a$10$sxGcXNaIN2aI5C3z5knUJuLmkSbYeU.Gp/Akh/06VcyZAJlm1S9Uu', 'INVITADO', 'N', NULL),
+(10, 'invitado7@gmail.com', '$2a$10$Y4MLKEh9Ut3cUHDRNqGYh.WIUNiVVU5rM5mF/XnINyIbeBTkgv.Sm', 'INVITADO', 'N', NULL);
 
 --
 -- Índices para tablas volcadas
@@ -415,19 +427,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `actividades`
 --
 ALTER TABLE `actividades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `asignaciones_habitaciones`
 --
 ALTER TABLE `asignaciones_habitaciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `asistencias_actividades`
 --
 ALTER TABLE `asistencias_actividades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `habitaciones`
@@ -439,7 +451,7 @@ ALTER TABLE `habitaciones`
 -- AUTO_INCREMENT de la tabla `lista_invitados_previa`
 --
 ALTER TABLE `lista_invitados_previa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `opiniones_feedback`
@@ -469,13 +481,13 @@ ALTER TABLE `premios`
 -- AUTO_INCREMENT de la tabla `reservas_hotel`
 --
 ALTER TABLE `reservas_hotel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
